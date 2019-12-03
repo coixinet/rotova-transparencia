0e768b78228de60f2398f46315d403af3ffcba69
monjo <jvmonjo@gmail.com>
2019-12-03 12:18:40 +0100

    FIX memory leack

--- configs/setenv.sh
+++ configs/setenv.sh
@@ -1,1 +1,2 @@
-CATALINA_OPTS="$CATALINA_OPTS -server -Dfile.encoding=UTF8 -Xms1g -Xmx4g -XX:NewSize=200m -XX:MaxNewSize=200m -XX:-UseGCOverheadLimit -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:SurvivorRatio=20 -XX:ParallelGCThreads=2 -Djava.net.preferIPv4Stack=true"
+CATALINA_OPTS="$CATALINA_OPTS -server -Dfile.encoding=UTF8 -Xms4096m -Xmx4096m -Xms1g -Xmx4g -XX:NewSize=200m -XX:MaxNewSize=200m -XX:-UseGCOverheadLimit -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:SurvivorRatio=20 -XX:ParallelGCThreads=2 -Djava.net.preferIPv4Stack=true"
+JAVA_OPTS="-Xms4096m -Xmx4096m"
